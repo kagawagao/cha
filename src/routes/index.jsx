8f56d9157be34a970fe9a08159df835a9250d185
kagawagao <kingsongao1221@gmail.com>
2018-12-19 11:15:16 +0800

    feat: use react.lazy instead

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,11 @@
+import React, { lazy } from 'react'
+
+const Home = lazy(() => import('app/home'))
+
 export default [{
   path: '/',
   exact: true,
-  getComponent: () => import('../app/home'),
+  component: (props) => <Home {...props} />,
   meta: {
     name: 'home'
   }
